3271cd769857277939c4d606b7ca2db6843ae291
José Inácio <135560987+Jose-Tchingunji-Inacio@users.noreply.github.com>
2024-07-27 07:56:12 +0100

    c00

---
+++ ft_putnbr.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('-');
+		if (n == -2147483648)
+		{
+			ft_putchar('2');
+			n = -147483648;
+		}
+		n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	ft_putchar(n % 10 + '0');
+}
